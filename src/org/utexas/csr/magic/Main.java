4587387b577b650f2d70215acd304135986206bf
Brent Porter <webotter@gmail.com>
2018-12-11 22:07:32 -0600

    Update Main.java

--- src/org/utexas/csr/magic/Main.java
+++ src/org/utexas/csr/magic/Main.java
@@ -1,20 +1,30 @@
-package org.utexas.csr.magic;
+ package org.utexas.csr.magic;
 
 import org.utexas.csr.magic.ai.Routine;
 import org.utexas.csr.magic.ai.Routines;
 
 import javax.swing.*;
-import java.util.Random;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Main {
+    private JPanel panel1;
+    private JPanel mainPanel;
+    private JPanel canvas;
     private static Random random = new Random();
 
     public static void main(String[] args) {
 
+        /*for(int i=0;i<10;i++) {
+            //System.out.println(random.nextInt(10) + 1);
+            System.out.println((random.nextInt(25) + 1)+ " " + (random.nextInt(25)+1));
+        }
+        */
+        System.out.println((random.nextInt(2)+1 )+ " " + (random.nextInt(2)+1));
         Board board = new Board(25, 25);
-        Droid droid1 = new Droid("Droid_1", 5, 2, 10, random.nextInt(3)+1, random.nextInt(3)+1);
-        Droid droid2 = new Droid("Droid_2", 6, 3, 10, random.nextInt(3)+1, random.nextInt(3)+1);
-        Droid droid3 = new Droid("Droid_3", 4, 5, 10, random.nextInt(3)+1, random.nextInt(3)+1);
+        Droid droid1 = new Droid("Droid_1", 5, 2, random.nextInt(6)+3, random.nextInt(2)+1, random.nextInt(4)+2);
+        Droid droid2 = new Droid("Droid_2", 6, 3, random.nextInt(6)+2, random.nextInt(2)+1, random.nextInt(4)+2);
+        Droid droid3 = new Droid("Droid_3", 4, 5, random.nextInt(6)+1, random.nextInt(2)+1, random.nextInt(4)+2);
 
         board.addDroid(droid1);
         board.addDroid(droid2);
@@ -28,15 +38,22 @@ public class Main {
         droid3.setRoutine(brain2);
         Routine brain1 = Routines.sequence(
                 Routines.moveTo(5, 10),
-                Routines.moveTo(15, 12),
+                Routines.moveTo(10, 12),
                 Routines.moveTo(2, 4)
         );
         droid1.setRoutine(brain1);
-
-        for (int i = 0; i < 20; i++) {
+        List<Droid> aryDroidStack = new ArrayList<>();
+        aryDroidStack.add(droid1);
+        aryDroidStack.add(droid2);
+        aryDroidStack.add(droid3);
+        for (int i = 0; i < 5; i++) {
             System.out.println(droid1.toString());
             System.out.println(droid2.toString());
             System.out.println(droid3.toString());
+/*            Collections.shuffle(aryDroidStack);
+            for(int j=0;j<3;j++){
+                aryDroidStack.get(j).update();
+            }*/
             droid1.update();
             droid2.update();
             droid3.update();
